e30ca90b85a7ef767118de41bc995f982b49d80d
DancyPh <phongnguyenn121@gmail.com>
2024-04-02 18:25:27 +0700

    fix compileSdk and targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.travelhotel"
         minSdk = 26
-        targetSdk = 34
+        this.targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
